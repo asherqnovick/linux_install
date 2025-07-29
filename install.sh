d278965e3880ce90603928305668f8cf311aec3f
asherqnovick <74268160+asherqnovick@users.noreply.github.com>
2025-07-28 21:45:06 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# sudo dpkg-reconfigure keyboard-configuration
-sudo truncate -s 0 /etc/issue
-sudo truncate -s 0 /etc/motd
-
-sudo bash -c 'echo deb http://ftp.us.debian.org/debian trixie main >> /etc/apt/sources.list'
-
+sudo apt -y update
+sudo apt -y upgrade
 
 sudo apt install -y git
 sudo apt install -y stow
 git clone https://github.com/asherqnovick/dotfiles_linux/ $HOME/dotfiles
 stow -d $HOME/dotfiles .
 
-
-sudo apt -y update
-
 sudo apt install -y software-properties-common
 sudo apt install -y zsh
 sudo apt install -y ripgrep
-sudo apt install -y neovim
 sudo apt install -y curl
 sudo apt install -y bat
 sudo apt install -y lf
@@ -30,12 +22,18 @@ sudo apt install -y yt-dlp
 sudo apt install -y gh
 sudo apt install -y golang
 
-sudo apt -y upgrade
-sudo apt -y clean
 sudo apt -y autoremove
+
+sudo bash -c 'echo deb http://ftp.us.debian.org/debian trixie main >> /etc/apt/sources.list'
+sudo apt -y update
+sudo apt install -y neovim
+
+sudo apt -y upgrade
 
 echo "exec i3" > $HOME/.initrc
 chsh -s /bin/zsh
+sudo truncate -s 0 /etc/issue
+sudo truncate -s 0 /etc/motd
 
 gh auth login
 gh auth setup-git
