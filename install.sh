e69eb9c396a1ec2348c02e48399592f1de8cbcc1
asherqnovick <asherqnovick@gmail.com>
2025-07-28 21:06:05 -0700

    added things

--- install.sh
+++ install.sh
@@ -7,17 +7,22 @@ sudo truncate -s 0 /etc/motd
 
 sudo bash -c 'echo deb http://ftp.us.debian.org/debian trixie main >> /etc/apt/sources.list'
 
+
+sudo apt install -y git
+sudo apt install -y stow
+git clone https://github.com/asherqnovick/dotfiles_linux/ | $HOME/dotfiles
+stow -d $HOME/dotfiles .
+
+
 sudo apt -y update
 
 sudo apt install -y software-properties-common
 sudo apt install -y zsh
 sudo apt install -y ripgrep
 sudo apt install -y neovim
-sudo apt install -y stow
 sudo apt install -y curl
 sudo apt install -y bat
 sudo apt install -y lf
-sudo apt install -y git
 sudo apt install -y fzf
 sudo apt install -y chromium-browser
 sudo apt install -y xinit
@@ -31,6 +36,8 @@ sudo apt install -y golang
 sudo apt -y upgrade
 sudo apt -y clean
 sudo apt -y autoremove
+
+echo "exec i3" > $HOME/.initrc
 
 gh auth login
 gh auth setup-git
